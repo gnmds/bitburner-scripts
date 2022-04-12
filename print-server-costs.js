c8ec020280bf1d6eddbda5a80d9482a5859d0e9d
Gabriel <gabriel@nagelmichaelsen.de>
2022-04-12 09:38:56 +0200

    shows if 25 servers can be bought

--- print-server-costs.js
+++ print-server-costs.js
@@ -9,7 +9,7 @@ export async function main(ns) {
         let costs = ns.nFormat(ns.getPurchasedServerCost(i) * ns.getPurchasedServerLimit(), "($000.000a");
         let costResult = "Costs: ".concat(costs);
 
-        let affordable = ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(i) ? true : false;
+        let affordable = ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(i) * 25 ? true : false;
         let affordableResult = "Affordable: ".concat(affordable);
 
         ns.tprint(ramResult.concat(" -- ", costResult, " -- ", affordableResult));
