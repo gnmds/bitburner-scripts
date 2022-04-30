c526ec1256beb16b11c05becd275f694d2a1d5a1
Gabriel <gabriel@nagelmichaelsen.de>
2022-04-30 18:38:58 +0200

    print-server-costs.js now shows current RAM size

--- print-server-costs.js
+++ print-server-costs.js
@@ -3,6 +3,8 @@ export async function main(ns) {
 
     for (let i = 1; i <= ns.getPurchasedServerMaxRam() + 1; i *= 2) {
 
+        let servers = ns.getPurchasedServers();
+
         let ram = ns.nFormat(i * 1000000000, "000.00b");
         let ramResult = "RAM: ".concat(ram);
 
@@ -12,7 +14,13 @@ export async function main(ns) {
         let affordable = ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(i) * 25 ? true : false;
         let affordableResult = "Affordable: ".concat(affordable);
 
-        ns.tprint(ramResult.concat(" -- ", costResult, " -- ", affordableResult));
+        let result = ramResult.concat(" -- ", costResult, " -- ", affordableResult);
+
+        if (i === ns.getServerMaxRam(servers[0])) {
+            result = result.concat(" -- current RAM Size");
+        }
+
+        ns.tprint(result);
     }
 
 }
